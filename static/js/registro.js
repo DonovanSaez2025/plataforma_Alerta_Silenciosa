a534db16ec22bfdc7041ac205b525b5257f00fc1
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-08-20 11:12:10 -0400

    registro

--- static/js/registro.js
+++ static/js/registro.js
@@ -1,28 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    const togglePassword = document.querySelector('#togglePassword');
-    const password = document.querySelector('#password');
-
-    togglePassword.addEventListener('click', function () {
-        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-        password.setAttribute('type', type);
-        this.classList.toggle('fa-eye-slash');
-    });
-
     const loginForm = document.getElementById('loginForm');
 
-    loginForm.addEventListener('submit', function (e) {
+    loginForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
         const email = document.getElementById('email').value.trim();
         const password = document.getElementById('password').value.trim();
+        const repassword = document.getElementById('repassword').value.trim();
+
 
         if (!email.endsWith('@liceovvh.cl') && !email.endsWith('@comeduc.cl')) {
-            alert('Correo no valido. Solo se permiten correos institucionales.');
+            alert('Correo no válido. Solo se permiten correos institucionales.');
             return;
-        }
-        window.location.href = 'inicio.html';
+        };
 
-        loginForm.reset();
+        if (password !== repassword) {
+            alert('Las contraseñas no coinciden.');
+            return;
+        };
+
+        window.location.href = "/pages/inicio.html";
+        return;
     });
 });
+
